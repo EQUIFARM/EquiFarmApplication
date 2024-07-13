3478be82d617626e72d1f6fa8143b0f0741bdfbd
johnokwanyo2016 <johnokwanyo2016@gmail.com>
2024-07-13 23:15:15 +0300

    changes

--- app/src/main/java/com/example/equifarmapplication/data/remote/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/equifarmapplication/data/remote/api/RetrofitInstance.kt
@@ -1,4 +1,4 @@
 package com.example.equifarmapplication.data.remote.api
 
-interface ApiService {
+class RetrofitInstance {
 }
--- app/src/main/java/com/example/equifarmapplication/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/equifarmapplication/data/repository/UserRepository.kt
@@ -1,4 +1,4 @@
 package com.example.equifarmapplication.data.repository
 
-class Repository {
+class UserRepository {
 }
