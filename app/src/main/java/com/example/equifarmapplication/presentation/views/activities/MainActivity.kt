d5354fffae1aed15ffce75752924badc49b79644
johnokwanyo2016 <johnokwanyo2016@gmail.com>
2024-09-17 16:35:35 +0300

    changes in fragments

--- app/src/main/java/com/example/equifarmapplication/presentation/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/equifarmapplication/presentation/views/activities/MainActivity.kt
@@ -2,14 +2,25 @@ package com.example.equifarmapplication.presentation.views.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.equifarmapplication.R
 import com.example.equifarmapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //initializing navController
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        //set up bottomnavigationviewwith navController
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
 
     }
 }
