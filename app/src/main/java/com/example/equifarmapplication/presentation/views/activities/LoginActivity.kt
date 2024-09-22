03e4ecd4d329e1f5c48464c661f09a50372a95d9
Allan Mathenge <allanmathenge67@gmail.com>
2024-09-22 15:53:55 +0300

    allan

--- app/src/main/java/com/example/equifarmapplication/presentation/views/activities/LoginActivity.kt
+++ app/src/main/java/com/example/equifarmapplication/presentation/views/activities/LoginActivity.kt
@@ -2,6 +2,12 @@ package com.example.equifarmapplication.presentation.views.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +27,41 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+       setupRegisterPrompt()
+    }
+
+    private fun setupRegisterPrompt() {
+        val registerPromptText = getString(R.string.register_prompt)
+        val spannableString = SpannableString(registerPromptText)
+
+        val startIndex = registerPromptText.indexOf("Register")
+        val endIndex = startIndex + "Register".length
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                // Show ProgressBar when Register is clicked
+                binding.progressBar.visibility = View.VISIBLE
+
+                // Start RegisterAgentActivity
+                val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false // Remove underline
+                ds.color = resources.getColor(R.color.primaryColor, null)
+            }
+        }
+
+        spannableString.setSpan(clickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvRegisterPrompt.text = spannableString
+        binding.tvRegisterPrompt.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Hide ProgressBar when returning to the activity
+        binding.progressBar.visibility = View.GONE
     }
 }
