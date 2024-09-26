55f848b3a529da12480f1894e7fed1a1bccfea41
Allan Mathenge <allanmathenge67@gmail.com>
2024-09-26 14:16:30 +0300

    allan

--- app/src/main/java/com/example/equifarmapplication/presentation/views/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/equifarmapplication/presentation/views/activities/RegisterActivity.kt
@@ -1,21 +1,63 @@
 package com.example.equifarmapplication.presentation.views.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.equifarmapplication.R
+import com.example.equifarmapplication.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        setupSigninPrompt()
+    }
+
+    private fun setupSigninPrompt() {
+        val loginPromptText = getString(R.string.loginPrompt)
+        val  spannableString = SpannableString(loginPromptText)
+
+        val startIndex = loginPromptText.indexOf("Sign In")
+        val endIndex = startIndex + "Sign In".length
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                // Show ProgressBar when Register is clicked
+                binding.progressBar.visibility = View.VISIBLE
+
+                // Start RegisterAgentActivity
+                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false // Remove underline
+                ds.color = resources.getColor(R.color.primaryColor, null)
+            }
+        }
+
+        spannableString.setSpan(clickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.loginPrompt.text = spannableString
+        binding.loginPrompt.movementMethod = LinkMovementMethod.getInstance()
     }
 }
